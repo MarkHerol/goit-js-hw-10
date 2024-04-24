5bfb6cfe3e8c682cb203a570963e62cda8b801f0
Mark Herol Sobretodo <herol.sobretodo@gmail.com>
2024-04-24 15:50:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,64 +1,75 @@
+import axios from "axios";
 
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+// Setting the API key in the HTTP header
+axios.defaults.headers.common["x-api-key"] = "your key";
 
-const breedSelectEl = document.querySelector(".breed-select");
-const catInfoEl = document.querySelector(".cat-info");
-const loaderEl = document.querySelector(".loader");
-const errorEl = document.querySelector(".error");
+// Fetching the list of breeds and returning a promise
+export function fetchBreeds() {
+  // Showing the loader and hiding the breed selector
+  document.querySelector(".breed-select").style.display = "none";
+  document.querySelector(".loader").style.display = "block";
 
-errorEl.classList.add("is-hidden");
+  return axios
+    .get("https://api.thecatapi.com/v1/breeds")
+    .then((response) => {
+      // Hiding the loader and showing the breed selector
+      document.querySelector(".loader").style.display = "none";
+      document.querySelector(".breed-select").style.display = "block";
 
-//CREATE THE OPTIONS
-function chooseBreed() {
-	fetchBreeds()
-		.then((data) => {
-			loaderEl.classList.replace("loader", "is-hidden");
+      // Populating the breed selector with options
+      const breedSelect = document.querySelector(".breed-select");
+      breedSelect.innerHTML = "";
+      response.data.forEach((breed) => {
+        const option = document.createElement("option");
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
 
-			let optionsMarkup = data.map(({ name, id }) => {
-				return `<option value=${id}>${name}</option>`;
-				//<option value={catId} >Cat Name</option>
-			});
-			breedSelectEl.insertAdjacentHTML("beforeend", optionsMarkup);
-			breedSelectEl.classList.remove("is-hidden");
-		})
-		.catch(onError);
+      // Returning the list of breeds
+      return response.data;
+    })
+    .catch((error) => {
+      // Hiding the loader and showing the error message
+      document.querySelector(".loader").style.display = "none";
+      document.querySelector(".error").style.display = "block";
+
+      // Rejecting the promise with the error
+      return Promise.reject(error);
+    });
 }
 
-chooseBreed();
+// Fetching the cat data by breed ID and returning a promise
+export function fetchCatByBreed(breedId) {
+  // Showing the loader and hiding the cat info
+  document.querySelector(".cat-info").style.display = "none";
+  document.querySelector(".loader").style.display = "block";
 
-breedSelectEl.addEventListener("change", (e) => {
-	//show loader while loading.
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then((response) => {
+      // Hiding the loader and showing the cat info
+      document.querySelector(".loader").style.display = "none";
+      document.querySelector(".cat-info").style.display = "block";
 
-	loaderEl.classList.replace("is-hidden", "loader");
+      // Populating the cat info with the cat data
+      const catInfo = document.querySelector(".cat-info");
+      catInfo.innerHTML = `
+        <h2>${response.data[0].breeds[0].name}</h2>
+        <p>${response.data[0].breeds[0].description}</p>
+        <p>Temperament: ${response.data[0].breeds[0].temperament}</p>
+        <img src="${response.data[0].url}" alt="${response.data[0].breeds[0].name}">
+      `;
 
-	//hide select element and cat info while loading.
+      // Returning the cat data
+      return response.data[0];
+    })
+    .catch((error) => {
+      // Hiding the loader and showing the error message
+      document.querySelector(".loader").style.display = "none";
+      document.querySelector(".error").style.display = "block";
 
-	catInfoEl.classList.add("is-hidden");
-
-	let breedId = e.target.value;
-	fetchCatByBreed(breedId)
-		.then((data) => {
-			const { url, breeds } = data[0];
-			const { name, description, temperament } = breeds[0];
-
-			catInfoEl.innerHTML = `
-            <img src='${url}' alt='{name}' width="400"/>
-            <div class='box'>
-                <h2>${name}</h2>
-                <p>${description}</p>
-                <p>${temperament}</p>
-            </div>
-        `;
-			catInfoEl.classList.remove("is-hidden");
-			loaderEl.classList.add("is-hidden");
-		})
-		.catch(onError);
-});
-
-function onError() {
-	//Show error message
-	errorEl.classList.remove("is-hidden");
-
-	//hide select element
-	breedSelectEl.classList.add("is-hidden");
+      // Rejecting the promise with the error
+      return Promise.reject(error);
+    });
 }
